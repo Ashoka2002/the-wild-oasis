e5b7bfe4686e3ed0426e5d9163694b4adb2fc5a7
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-11 18:50:12 +0530

    creating database using supabase

---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = "https://hlafvlrngevjbhugkhxh.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsYWZ2bHJuZ2V2amJodWdraHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwMjM5MzIsImV4cCI6MjAxMjU5OTkzMn0.2dGRFmtruQymMBRxUXa667q1czTuFLPXbEQ3WwYW7Dw";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
