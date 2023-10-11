4997493682ddcb57007f8ba82b0751cc8d1b21a5
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-11 09:08:18 +0530

    creating routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,28 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DashBoard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+
 function App() {
-  return <div>Heallo react</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<DashBoard />} />
+        <Route path="account" element={<Account />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="cabins" element={<Cabins />} />
+        <Route path="login" element={<Login />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
