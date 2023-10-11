0c69c75c82ba6077459490fb338ece8576c40fe6
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-11 19:01:19 +0530

    not much change

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,3 +1,4 @@
+import styled from "styled-components";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 
@@ -6,6 +7,10 @@ function Cabins() {
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
       <p>TEST</p>
+      <img
+        src="https://hlafvlrngevjbhugkhxh.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg?t=2023-10-11T13%3A24%3A48.736Z"
+        alt="cabin"
+      />
     </Row>
   );
 }
