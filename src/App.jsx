dbff4e9dda6cd95bda30d6e9344a08892b3cf464
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-12 11:25:33 +0530

    adding toast

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import PageNotFound from "./pages/PageNotFound";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import AppLayout from "./ui/AppLayout";
+import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -42,6 +43,27 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
+
+      <Toaster
+        position="top-center"
+        gutter={12}
+        containerStyle={{ margin: "8px" }}
+        toastOptions={{
+          success: {
+            duration: 3000,
+          },
+          error: {
+            duration: 4000,
+          },
+          style: {
+            backgroundColor: "var(--color-grey-0)",
+            color: "var(--color-grey-700)",
+            fontSize: "16px",
+            maxWidth: "600px",
+            padding: "16px 24px",
+          },
+        }}
+      />
     </QueryClientProvider>
   );
 }
