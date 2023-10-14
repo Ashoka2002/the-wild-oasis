8833cbc8b7d0c5648f8ce67ae55766b0b2189962
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-14 17:58:56 +0530

    react-dom's portal

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -1,3 +1,4 @@
+import { createPortal } from "react-dom";
 import { HiXMark } from "react-icons/hi2";
 import styled from "styled-components";
 
@@ -51,7 +52,7 @@ const Button = styled.button`
 `;
 
 function Modal({ onClose, children }) {
-  return (
+  return createPortal(
     <Overlay>
       <StyledModal>
         <Button onClick={onClose}>
@@ -59,7 +60,8 @@ function Modal({ onClose, children }) {
         </Button>
         {children}
       </StyledModal>
-    </Overlay>
+    </Overlay>,
+    document.body
   );
 }
 
