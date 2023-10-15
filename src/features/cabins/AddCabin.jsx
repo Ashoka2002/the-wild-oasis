2e86aedcc7dbe19db0db0e7d5f7cb440f8d5e213
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-15 07:28:58 +0530

    creating table using compound component

--- src/features/cabins/AddCabin.jsx
+++ src/features/cabins/AddCabin.jsx
@@ -2,23 +2,6 @@ import Button from "../../ui/Button";
 import CreateCabinForm from "./CreateCabinForm";
 import Modal from "../../ui/Modal";
 
-// function AddCabin() {
-//   const [isModalOpen, setIsModalOpen] = useState(false);
-
-//   return (
-//     <>
-//       <Button onClick={() => setIsModalOpen((show) => !show)}>
-//         Toggle Form
-//       </Button>
-//       {isModalOpen && (
-//         <Modal onClose={() => setIsModalOpen(false)}>
-//           <CreateCabinForm onCloseModal={() => setIsModalOpen(false)} />
-//         </Modal>
-//       )}
-//     </>
-//   );
-// }
-
 function AddCabin() {
   return (
     <div>
