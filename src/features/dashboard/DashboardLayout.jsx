0461052bf837d8dfeee2a2220459a5acf92e97f2
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-19 09:51:23 +0530

    dashboard layout

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -6,3 +6,16 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  return (
+    <StyledDashboardLayout>
+      <div>Statics</div>
+      <div>Today's Activity</div>
+      <div>Chart stay Duration</div>
+      <div>Chart sales</div>
+    </StyledDashboardLayout>
+  );
+}
+
+export default DashboardLayout;
