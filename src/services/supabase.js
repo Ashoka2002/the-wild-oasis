7e84f6d56cf4aa3c8520212c533555ebfecd219d
Ashok Choudhary <ashoka8769@gmail.com>
2023-10-31 12:53:50 +0530

    api security

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://hlafvlrngevjbhugkhxh.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsYWZ2bHJuZ2V2amJodWdraHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwMjM5MzIsImV4cCI6MjAxMjU5OTkzMn0.2dGRFmtruQymMBRxUXa667q1czTuFLPXbEQ3WwYW7Dw";
+const supabaseKey = process.env.REACT_APP_SUPABASE_API;
+// "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhsYWZ2bHJuZ2V2amJodWdraHhoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcwMjM5MzIsImV4cCI6MjAxMjU5OTkzMn0.2dGRFmtruQymMBRxUXa667q1czTuFLPXbEQ3WwYW7Dw";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
